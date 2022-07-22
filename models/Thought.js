20c6681dbb3b7acb4ce93c0070bd99c97905cd0e
Jide Adesanya <Jide1012@yahoo.com>
2022-07-22 11:58:48 -0500

    updated routes

--- models/Thought.js
+++ models/Thought.js
@@ -1,4 +1,4 @@
-const dateFormat = require('../utils/dateFormat.js');
+const dataFormat = require('../utils/dataFormat.js');
 const { Schema, model, Types } = require('mongoose');
 
 const ReactionSchema = new Schema(
@@ -31,7 +31,7 @@ const ThoughtSchema = new Schema(
             default: Date.now,
             get: createdAtVal => dateFormat(createdAtVal)
         },
-        username: { 
+        username: {
             type: String,
             required: true
         },
@@ -53,7 +53,7 @@ const ThoughtSchema = new Schema(
     }
 );
 
-ThoughtSchema.virtual('reactionCount').get(function() {
+ThoughtSchema.virtual('reactionCount').get(function () {
     return this.reactions.length;
 });
 
